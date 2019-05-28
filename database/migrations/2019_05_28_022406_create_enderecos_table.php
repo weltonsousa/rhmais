37a8b7b621e774f7023aa8883fd39ab1e549f539
Welton Sousa <weltonsousa@gmail.com>
2019-05-27 23:52:46 -0300

    schema database

---
+++ database/migrations/2019_05_28_022406_create_enderecos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->increments('id_endereco');
+            $table->timestamps();
+            $table->String(`endereco`);
+            $table->String(`numero`);
+            $table->String(`bairro`);
+            $table->String(`cidade`);
+            $table->String('estado');
+          });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
